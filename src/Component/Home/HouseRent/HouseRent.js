3e78988a0687635c57146905744dd27b86c11e21
nanditamandal <nanditamandal.19@gmial.com>
2020-11-16 23:56:38 +0600

    add home

--- src/Component/Home/HouseRent/HouseRent.js
+++ src/Component/Home/HouseRent/HouseRent.js
@@ -12,42 +12,125 @@ import { Link } from 'react-router-dom';
 import "./HouseRent.css"
 
 const HouseRent = () => {
+    const [apartment, setApartment]= useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/allApartment')
+        .then(res=>res.json())
+        .then(data=>
+                    setApartment(data)
+        )
+
+
+    }, [])
 
     const hrData = [
         {
-            title: 'Epic Huda Palacio',
-            img: House1,
-            price: '$356'
+            title: 'Washington Avenue',
+            img: "https://i.ibb.co/KWD7FFZ/Rectangle-394.png",
+            price: '$194',
+            location: 'Nasirabad H/S, Chattogram',
+            bathRoom:'2 Bathroom',
+            bedRoom: '3 Bedrooms',
+            titleDetails: '3000 sq-ft, Bedroom, semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.',
+            propertyDetails: 'Address & Area : Rangs Malancha, House -68 , Road -6A(Dead End Road), Dhanmondi Residential Area',
+            priceDetails: 'Rent/Month: $194(negotiable) Service Charge: 8000/= tk per month, subject to change Security Deposit: 3 months rent Flat Release Policy: 3 months earlier notice required',
+            img1: "https://i.ibb.co/DWMXMf4/Rectangle-410.png",
+            img2: "https://i.ibb.co/nnrCmJq/Rectangle-409.png",
+            img3: "https://i.ibb.co/P6K8HM1/Rectangle-408.png" ,
+            img4: "https://i.ibb.co/n73CPLM/Rectangle-407.png"
+        },
+        {
+            title: 'Family Apartment Three',
+            img: "https://i.ibb.co/Hq3cSL3/Rectangle-396.png" ,
+            price: '$356',
+            location: 'Nasirabad H/S, Chattogram',
+            bathRoom:'2 Bathroom',
+            bedRoom: '3 Bedrooms',
+            titleDetails: '3000 sq-ft, Bedroom, semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.',
+            propertyDetails: 'Address & Area : Rangs Malancha, House -68 , Road -6A(Dead End Road), Dhanmondi Residential Area',
+            priceDetails: 'Rent/Month: $194(negotiable) Service Charge: 8000/= tk per month, subject to change Security Deposit: 3 months rent Flat Release Policy: 3 months earlier notice required',
+            img1: "https://i.ibb.co/DWMXMf4/Rectangle-410.png",
+            img2: "https://i.ibb.co/nnrCmJq/Rectangle-409.png",
+            img3: "https://i.ibb.co/P6K8HM1/Rectangle-408.png" ,
+            img4: "https://i.ibb.co/n73CPLM/Rectangle-407.png"
+        },
+        {
+            title: 'Gorgeous house',
+            img: "https://i.ibb.co/XDgTJG5/Rectangle-398.png" ,
+            price: '$256',
+            location: 'Nasirabad H/S, Chattogram',
+            bathRoom:'2 Bathroom',
+            bedRoom: '3 Bedrooms',
+            titleDetails: '3000 sq-ft, Bedroom, semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.',
+            propertyDetails: 'Address & Area : Rangs Malancha, House -68 , Road -6A(Dead End Road), Dhanmondi Residential Area',
+            priceDetails: 'Rent/Month: $194(negotiable) Service Charge: 8000/= tk per month, subject to change Security Deposit: 3 months rent Flat Release Policy: 3 months earlier notice required',
+            img1: "https://i.ibb.co/DWMXMf4/Rectangle-410.png",
+            img2: "https://i.ibb.co/nnrCmJq/Rectangle-409.png",
+            img3: "https://i.ibb.co/P6K8HM1/Rectangle-408.png" ,
+            img4: "https://i.ibb.co/n73CPLM/Rectangle-407.png"
+        },
+        {
+            title: 'Luxury villa',
+            img: "https://i.ibb.co/6m6ZPMt/Rectangle-405.png",
+            price: '$346',
+            location: 'Nasirabad H/S, Chattogram',
+            bathRoom:'2 Bathroom',
+            bedRoom: '3 Bedrooms',
+            titleDetails: '3000 sq-ft, Bedroom, semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.',
+            propertyDetails: 'Address & Area : Rangs Malancha, House -68 , Road -6A(Dead End Road), Dhanmondi Residential Area',
+            priceDetails: 'Rent/Month: $194(negotiable) Service Charge: 8000/= tk per month, subject to change Security Deposit: 3 months rent Flat Release Policy: 3 months earlier notice required',
+            img1: "https://i.ibb.co/DWMXMf4/Rectangle-410.png",
+            img2: "https://i.ibb.co/nnrCmJq/Rectangle-409.png",
+            img3: "https://i.ibb.co/P6K8HM1/Rectangle-408.png" ,
+            img4: "https://i.ibb.co/n73CPLM/Rectangle-407.png"
         },
         {
             title: 'Epic Huda Palacio',
-            img: House2,
-            price: '$356'
+            img: "https://i.ibb.co/L1w34KC/Rectangle-403.png",
+            price: '$536',
+            location: 'Nasirabad H/S, Chattogram',
+            bathRoom:'2 Bathroom',
+            bedRoom: '3 Bedrooms',
+            titleDetails: '3000 sq-ft, Bedroom, semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.',
+            propertyDetails: 'Address & Area : Rangs Malancha, House -68 , Road -6A(Dead End Road), Dhanmondi Residential Area',
+            priceDetails: 'Rent/Month: $194(negotiable) Service Charge: 8000/= tk per month, subject to change Security Deposit: 3 months rent Flat Release Policy: 3 months earlier notice required',
+            img1: "https://i.ibb.co/DWMXMf4/Rectangle-410.png",
+            img2: "https://i.ibb.co/nnrCmJq/Rectangle-409.png",
+            img3: "https://i.ibb.co/P6K8HM1/Rectangle-408.png" ,
+            img4: "https://i.ibb.co/n73CPLM/Rectangle-407.png"
         },
         {
-            title: 'Epic Huda Palacio',
-            img: House3,
-            price: '$356'
-        },
-        {
-            title: 'Epic Huda Palacio',
-            img: House4,
-            price: '$356'
-        },
-        {
-            title: 'Epic Huda Palacio',
-            img: House5,
-            price: '$356'
-        },
-        {
-            title: 'Epic Huda Palacio',
-            img: House6,
-            price: '$356'
+            title: 'washington Avenue',
+            img: "https://i.ibb.co/TW51bKn/Rectangle-404.png",
+            price: '$283',
+            location: 'Nasirabad H/S, Chattogram',
+            bathRoom:'2 Bathroom',
+            bedRoom: '3 Bedrooms',
+            titleDetails: '3000 sq-ft, Bedroom, semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.',
+            propertyDetails: 'Address & Area : Rangs Malancha, House -68 , Road -6A(Dead End Road), Dhanmondi Residential Area',
+            priceDetails: 'Rent/Month: $194(negotiable) Service Charge: 8000/= tk per month, subject to change Security Deposit: 3 months rent Flat Release Policy: 3 months earlier notice required',
+            img1: "https://i.ibb.co/DWMXMf4/Rectangle-410.png",
+            img2: "https://i.ibb.co/nnrCmJq/Rectangle-409.png",
+            img3: "https://i.ibb.co/P6K8HM1/Rectangle-408.png" ,
+            img4: "https://i.ibb.co/n73CPLM/Rectangle-407.png"
         }
 
     ]
 
+    const handelAdd=()=>{
+        fetch('http://localhost:5000/addAll',{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json',
+                        'Accept': 'application/json' },
+            body: JSON.stringify(hrData)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+                    
+            data? alert("Service added"): console.log(data) 
+        })
 
+    }
 
 
     return (
@@ -57,12 +140,13 @@ const HouseRent = () => {
                     <small className="color-brand" style={{fontSize:"16px", height:"24px"}}>House Rent <br/> </small>
                    <span style={{color:"#16322E", fontWeight:"bold", fontFamily:"poppins", fontSize:"34px"}}>Discover the latest Rent <br/> available today</span> 
                 </p>
+                
             </div>
 
             <div className="container">
                 <div className="row">
                     {
-                        hrData.map(house=> 
+                       apartment && apartment.map(house=> 
                             <div className="col-md-4">
                                 <div className="card card_shadow" style={{marginBottom: "29px", border: "none"}}>
                                     <img src={house.img} className="card-img-top rentimg" alt="..."/>
@@ -70,16 +154,16 @@ const HouseRent = () => {
                                         <h5 className="card-title" style={{color: '#16322E', fontSize: '24px', fontWeight: 'bold'}}>{house.title}</h5>
                                         <div className="card-text">
                                             <img src={location} alt="" height="20" width="15"/>
-                                            <span style={{color: '#747474', marginBottom:'8px'}}> Nasirabad H/S, Chattogram</span>   
+                                            <span style={{color: '#747474', marginBottom:'8px'}}>{house.location}</span>   
                                         </div>
                                         <div style={{display: 'flex',marginBottom:'42px'}}>
                                             <div>
                                                 <img src={bedroom} alt="" height="20" width="15"/>
-                                                <span style={{color: '#747474'}}> 3 bedrooms</span>
+                                                <span style={{color: '#747474'}}>{house.bedRoom}</span>
                                             </div>
                                             <div style={{marginLeft: '60px'}}>
                                                 <img src={bathroom} alt="" height="20" width="15"/>
-                                                <span style={{color: '#747474'}}> 2 bathrooms</span>
+                                                <span style={{color: '#747474'}}>{house.bathRoom}</span>
                                             </div>
                                         </div>
                                         <div style={{display: 'flex'}}>
